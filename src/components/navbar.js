c32983f97941c5c4553b573416ec7b7ce9dc9f37
fj <castelloiv@gmail.com>
2020-05-01 14:08:58 -0400

    Active link styling added

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -21,66 +21,148 @@ function Navbar() {
         >
           <ul>
             <li onClick={() => setNavnames(false)}>
-              <Link to="/">Home</Link>
+              <Link
+                activeClassName={Style.active}
+                to="/"
+                activeClassName={Style.active}
+              >
+                Home
+              </Link>
             </li>
 
             <li>
-              <Link className={Style.darrow} to="/basics">
+              <Link
+                activeClassName={Style.active}
+                className={Style.darrow}
+                to="/basics"
+              >
                 Basics
                 <div className={Style.drop}>
-                  <Link to="/whatis">What is HER2?</Link>
-                  <Link to="/">Stages</Link>
-                  <Link to="/">History</Link>
-                  <Link to="/">HER2+ (Positive)</Link>
-                  <Link to="/">HER2- (Negative)</Link>
-                  <Link to="/">link6</Link>
+                  <Link
+                    activeClassName={Style.active}
+                    activeClassName={Style.active}
+                    to="/whatis"
+                  >
+                    What is HER2?
+                  </Link>
+                  <Link activeClassName={Style.active} to="/nope">
+                    Stages
+                  </Link>
+                  <Link activeClassName={Style.active} to="/nope">
+                    History
+                  </Link>
+                  <Link activeClassName={Style.active} to="/nope">
+                    HER2+ (Positive)
+                  </Link>
+                  <Link activeClassName={Style.active} to="/nope">
+                    HER2- (Negative)
+                  </Link>
+                  <Link activeClassName={Style.active} to="/nope">
+                    link6
+                  </Link>
                 </div>
               </Link>
             </li>
             <li>
-              <Link className={Style.darrow} to="/traditional">
+              <Link
+                activeClassName={Style.active}
+                className={Style.darrow}
+                to="/traditional"
+              >
                 Traditional
                 <div className={Style.drop}>
-                  <Link to="/">Chemotherapy</Link>
-                  <Link to="/">Immunotherapy</Link>
-                  <Link to="/">Surgery</Link>
-                  <Link to="/">Radiation</Link>
-                  <Link to="/">Herceptin (Trastuzumab)</Link>
-                  <Link to="/">Perjeta (Pertuzumab)</Link>
-                  <Link to="/">Kadcyla</Link>
-                  <Link to="/">Nerlynx (Neratinib)</Link>
-                  <Link to="/">Tykerb (Lapatinib)</Link>
+                  <Link activeClassName={Style.active} to="/nope">
+                    Chemotherapy
+                  </Link>
+                  <Link activeClassName={Style.active} to="/nope">
+                    Immunotherapy
+                  </Link>
+                  <Link activeClassName={Style.active} to="/nope">
+                    Surgery
+                  </Link>
+                  <Link activeClassName={Style.active} to="/nope">
+                    Radiation
+                  </Link>
+                  <Link activeClassName={Style.active} to="/nope">
+                    Herceptin (Trastuzumab)
+                  </Link>
+                  <Link activeClassName={Style.active} to="/nope">
+                    Perjeta (Pertuzumab)
+                  </Link>
+                  <Link activeClassName={Style.active} to="/nope">
+                    Kadcyla
+                  </Link>
+                  <Link activeClassName={Style.active} to="/nope">
+                    Nerlynx (Neratinib)
+                  </Link>
+                  <Link activeClassName={Style.active} to="/nope">
+                    Tykerb (Lapatinib)
+                  </Link>
                 </div>
               </Link>
             </li>
             <li>
-              <Link className={Style.darrow} to="/alternative">
+              <Link
+                activeClassName={Style.active}
+                className={Style.darrow}
+                to="/alternative"
+              >
                 Alternative
                 <div className={Style.drop}>
-                  <Link to="/">Clinical Trials</Link>
-                  <Link to="/">Fake Treatments</Link>
-                  <Link to="/">link3</Link>
-                  <Link to="/">link4</Link>
-                  <Link to="/">link5</Link>
-                  <Link to="/">link6</Link>
+                  <Link activeClassName={Style.active} to="/nope">
+                    Clinical Trials
+                  </Link>
+                  <Link activeClassName={Style.active} to="/nope">
+                    Fake Treatments
+                  </Link>
+                  <Link activeClassName={Style.active} to="/nope">
+                    link3
+                  </Link>
+                  <Link activeClassName={Style.active} to="/nope">
+                    link4
+                  </Link>
+                  <Link activeClassName={Style.active} to="/nope">
+                    link5
+                  </Link>
+                  <Link activeClassName={Style.active} to="/nope">
+                    link6
+                  </Link>
                 </div>
               </Link>
             </li>
             <li>
-              <Link className={Style.darrow} to="/blogposts">
+              <Link
+                activeClassName={Style.active}
+                className={Style.darrow}
+                to="/blogposts"
+              >
                 Latest{" "}
                 <div className={Style.drop}>
-                  <Link to="/">link1</Link>
-                  <Link to="/">link2</Link>
-                  <Link to="/">link3</Link>
-                  <Link to="/">link4</Link>
-                  <Link to="/">link5</Link>
-                  <Link to="/">link6</Link>
+                  <Link activeClassName={Style.active} to="/nope">
+                    link1
+                  </Link>
+                  <Link activeClassName={Style.active} to="/nope">
+                    link2
+                  </Link>
+                  <Link activeClassName={Style.active} to="/nope">
+                    link3
+                  </Link>
+                  <Link activeClassName={Style.active} to="/nope">
+                    link4
+                  </Link>
+                  <Link activeClassName={Style.active} to="/nope">
+                    link5
+                  </Link>
+                  <Link activeClassName={Style.active} to="/nope">
+                    link6
+                  </Link>
                 </div>
               </Link>
             </li>
             <li>
-              <Link to="/about">About</Link>
+              <Link activeClassName={Style.active} to="/about">
+                About
+              </Link>
             </li>
           </ul>
         </nav>
